6e88a5a2b667c91a4b5bac5cc01a65257f00e97a
Shemuel2060 <2060newty@gmail.com>
2024-02-08 11:43:51 +0300

    created models:Rooms, Posts, Settings and migrated

--- parent_hub/Rooms/admin.py
+++ parent_hub/Rooms/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Rooms, Settings, Posts
 # Register your models here.
+
+admin.site.register([Rooms,Settings,Posts])
--- parent_hub/Rooms/models.py
+++ parent_hub/Rooms/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Rooms(models.Model):
+    """The discussion room"""
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    creation_date = models.DateTimeField(auto_now=True)
+    updated_date = models.DateTimeField(auto_now_add=True)
+    participants = ... # a list of users
+    # with another model
+    posts = models.ForeignKey("Posts", on_delete=models.DO_NOTHING)
+    settings = models.ForeignKey("Settings", on_delete=models.DO_NOTHING)
+    
+
+
+class Posts(models.Model):
+    """Messages in the discussion room"""
+    ...
+
+class Settings(models.Model):
+    """defines settings for a discussion room"""
+    ...
