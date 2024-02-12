38549ba322f2036ae72aac34f380e73bd6d6806f
Shemuel2060 <2060newty@gmail.com>
2024-02-12 20:12:03 +0300

    styled the navbar

--- parent_hub/parent_hub/settings.py
+++ parent_hub/parent_hub/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static' # path to the static files.
+    BASE_DIR / 'static' # path to the static files.   
 ]
 
 # Default primary key field type
