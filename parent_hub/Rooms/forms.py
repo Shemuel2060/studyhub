0ecf2467ddc0b6417b540890546acb77fa578a96
Shemuel2060 <2060newty@gmail.com>
2024-02-12 10:35:44 +0300

    including all fields in a model for a form and also excluding some

--- parent_hub/Rooms/forms.py
+++ parent_hub/Rooms/forms.py
@@ -6,11 +6,15 @@ class createRoomForm(forms.ModelForm):
     class Meta:
         """contents"""
         model = Room # the model associated with the form.
-        fields = [
-            'host', # room creator
-            'name', # name of room
-            'description', # short desc about the room
-            'topic' # topic emphasis for the room
-        ]
+        # Approach 1: include just selected fields
+        # fields = [
+        #     'host', # room creator
+        #     'name', # name of room
+        #     'description', # short desc about the room
+        #     'topic' # topic emphasis for the room
+        # ]
+        # Approach 2: include all fields, but exclude some
+        fields = '__all__' # includes all
+        exclude = ['host', 'participants'] # excludes the listed ones.
 
 # add other forms here...
